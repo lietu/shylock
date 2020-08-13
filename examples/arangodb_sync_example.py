ed1aa48d5e619737455f65cc0ea18a704301d421
Joakim Nordling <joakim.nordling@digitalliving.fi>
2020-08-13 16:19:43 +0300

    Add support for ArangoDB

---
+++ examples/arangodb_sync_example.py
@@ -0,0 +1,47 @@
+from time import time
+
+from arango import ArangoClient
+from shylock import configure, Lock, ShylockPythonArangoBackend
+
+HOSTS = "http://localhost:8529"
+USERNAME = "root"
+PASSWORD = ""
+
+
+def main():
+    print("Start")
+
+    client = ArangoClient(hosts=HOSTS)
+    db = client.db("shylock_test", username=USERNAME, password=PASSWORD)
+    configure(ShylockPythonArangoBackend.create(db, "shylock"))
+    lock_name = "test-lock"
+
+    test_lock = Lock(lock_name)
+    try:
+        with Lock(lock_name):
+            print("Got lock")
+            print("Testing re-lock")
+            assert not test_lock.acquire(False)
+            raise ValueError()
+    except ValueError:
+        print("Caught exception, lock should be released")
+
+    assert test_lock.acquire(False)
+    test_lock.release()
+
+    print("Testing automatic release, this might take a while.")
+
+    # Test automatic release
+    start = time()
+    with test_lock:
+        lock2 = Lock(lock_name)
+        try:
+            lock2.acquire()
+            released = time() - start
+        finally:
+            lock2.release()
+    print(f"Lock automatically released after {released:.3f}s")
+
+
+if __name__ == "__main__":
+    main()
---
+++ shylock/backends/pythonarango.py
@@ -0,0 +1,104 @@
+from time import sleep
+from typing import Optional
+
+try:
+    from arango.database import StandardDatabase
+    from arango.collection import StandardCollection
+    from arango.exceptions import ArangoServerError
+except ImportError:
+    StandardDatabase = None
+    StandardCollection = None
+    ArangoServerError = None
+
+from shylock.backends import ShylockSyncBackend
+from shylock.exceptions import ShylockException
+
+DOCUMENT_TTL = 60 * 5  # 5min seems like a reasonable TTL
+POLL_DELAY = 1 / 16  # Some balance between high polling and high delay
+
+ERROR_ARANGO_UNIQUE_CONSTRAINT_VIOLATED = 1210
+
+
+class ShylockPythonArangoBackend(ShylockSyncBackend):
+    @staticmethod
+    def create(
+        db: StandardDatabase, collection_name: str = "shylock"
+    ) -> "ShylockPythonArangoBackend":
+        """"
+        Create and initialize the backend
+        :param db: An instance of arango.database.StandardDatabase connected to the desired database
+        :param collection_name: The name of the collection reserved for shylock
+        """
+        inst = ShylockPythonArangoBackend(db, collection_name)
+        inst._init_collection()
+        return inst
+
+    def acquire(self, name: str, block: bool = True) -> bool:
+        """
+        Try to acquire a lock, potentially wait until it's available
+        :param name: Name of the lock
+        :param block: Wait for lock
+        :return: If lock was successfully acquired - always True if block is True
+        """
+        while True:
+            try:
+                self._db.aql.execute(
+                    """
+                    INSERT {
+                      "name": @name,
+                      "expiresAt": DATE_NOW() / 1000 + @ttl
+                    } IN @@collection
+                    """,
+                    bind_vars={
+                        "name": name,
+                        "ttl": DOCUMENT_TTL,
+                        "@collection": self._collection_name,
+                    },
+                )
+                return True
+            except ArangoServerError as err:
+                if err.error_code == ERROR_ARANGO_UNIQUE_CONSTRAINT_VIOLATED:
+                    if not block:
+                        return False
+                    sleep(POLL_DELAY)
+                else:
+                    raise
+
+    def release(self, name: str):
+        """
+        Release a given lock
+        :param name: Name of the lock
+        """
+        self._db.aql.execute(
+            """
+            FOR l IN @@collection
+                FILTER l.name == @name
+                REMOVE l IN @@collection
+            """,
+            bind_vars={"name": name, "@collection": self._collection_name},
+        )
+
+    @staticmethod
+    def _check():
+        if StandardDatabase is None:
+            raise ShylockException(
+                "No python-arango driver available. Cannot use Shylock with PythonArango backend without it."
+            )
+
+    def __init__(self, db: StandardDatabase, collection_name: str = "shylock"):
+        self._check()
+        self._db: StandardDatabase = db
+        self._coll: Optional[StandardCollection] = None
+        self._collection_name: str = collection_name
+
+    def _init_collection(self):
+        """
+        Ensure the collection is ready for our use
+        """
+        if self._db.has_collection(self._collection_name):
+            self._coll = self._db.collection(self._collection_name)
+        else:
+            self._coll = self._db.create_collection(self._collection_name)
+
+        self._coll.add_persistent_index(fields=["name"], unique=True)
+        self._coll.add_ttl_index(fields=["expiresAt"], expiry_time=0)
